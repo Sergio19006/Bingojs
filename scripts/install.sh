a306def15c1dff307f4e9236f487dc3da60c1cb7
sergio19006 <sergiodphn@gmail.com>
2019-06-18 20:38:08 +0100

    Trying again

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,9 @@
 
 echo "Importing SSH key"
 
-echo "REMOTE_PORT = ${REMOTE_PORT}"
+echo "REMOTE_HOST = ${REMOTE_HOST}"
 echo "REMOTE_USER = ${REMOTE_USER}"
+echo "REMOTE_PATH = ${REMOTE_PATH}"
 
 openssl aes-256-cbc -K $encrypted_83183f8e0998_key -iv $encrypted_83183f8e0998_iv
   -in deploy.enc -out ~\/claves/deploy -d
