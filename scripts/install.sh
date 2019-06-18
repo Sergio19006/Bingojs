b62b01d72103e2903e6339e564df548f0ebb268a
sergio19006 <sergiodphn@gmail.com>
2019-06-18 22:33:58 +0100

    builded

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ echo "REMOTE_HOST = ${REMOTE_HOST}"
 echo "REMOTE_USER = ${REMOTE_USER}"
 echo "REMOTE_PATH = ${REMOTE_PATH}"
 
-openssl aes-256-cbc -K $encrypted_83183f8e0998_key -iv $encrypted_83183f8e0998_iv -in deploy.enc -out ~\/claves/ -d
+openssl aes-256-cbc -K $encrypted_83183f8e0998_key -iv $encrypted_83183f8e0998_iv -in deploy.enc -out ~\/claves/deploy -d
 
 eval `$(ssh-agent -s)`
 
