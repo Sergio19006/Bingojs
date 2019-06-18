40989b90bd10076909c176e8c795e32cafe3d487
sergio19006 <sergiodphn@gmail.com>
2019-06-18 23:23:57 +0100

    Final

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-COMMAND="cd ${REMOTE_PATH} && git pull && git checkout deployDO && cp build /var/www/html/"
+COMMAND="cd ${REMOTE_PATH} && git pull && git checkout deployDO && cp build/* /var/www/html/"
 
 ssh -o StrictHostKeyChecking=no -i deploy -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
