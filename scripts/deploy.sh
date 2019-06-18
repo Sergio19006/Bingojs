4c6eb2e056b362ec6fba45fd82146bec4013fdfd
sergio19006 <sergiodphn@gmail.com>
2019-06-18 20:28:23 +0100

    Modified  deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+
+COMMAND= "cd ${REMOTE_PATH} && git pull"
+
+ssh -o StrictHostKeyChecking=no -i deploy -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
--- scripts/install.sh
+++ scripts/install.sh
@@ -5,13 +5,12 @@ echo "Importing SSH key"
 
 echo "REMOTE_PORT = ${REMOTE_PORT}"
 echo "REMOTE_USER = ${REMOTE_USER}"
-echo "REMOTE_HOST = ${REMOTE_HOST}"
 
 openssl aes-256-cbc -K $encrypted_83183f8e0998_key -iv $encrypted_83183f8e0998_iv
   -in deploy.enc -out ~\/claves/deploy -d
 
 eval `$(ssh-agent -s)`
 
-chmod 630 deploy
+chmod 600 deploy
 
 ssh-add deploy
